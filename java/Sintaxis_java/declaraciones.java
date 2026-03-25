868d9d6c919c75e186ae1434c58054b5fee0b2c8
Agustin Enrique Cabezas Yanguma <agustincabezas1454@gmail.com>
2026-03-24 22:54:49 -0500

    Declaraciones Java

---
+++ java/Sintaxis_java/declaraciones.java
@@ -0,0 +1,11 @@
+package java.Sintaxis_java;
+
+public class declaraciones {
+    public static void main(String[] args) {
+        System.out.println("Java is fun!") // Error siempre se finaliza con ;
+
+        System.out.println("Hello World!"); // Se imprime hello world en la terminal
+        System.out.println("Have a good day!"); // se imprime Have a good day! en la terminal
+        System.out.println("Learning Java is fun!"); // se imprime Learning Java is fun! en la terminal 
+    }
+}
