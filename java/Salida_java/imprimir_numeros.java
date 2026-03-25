8e73f648354037e3ce89c8685039993984bc0f4c
Agustin Enrique Cabezas Yanguma <agustincabezas1454@gmail.com>
2026-03-24 23:04:20 -0500

    Imprimir numeros

---
+++ java/Salida_java/imprimir_numeros.java
@@ -0,0 +1,16 @@
+package java.Salida_java;
+
+public class imprimir_numeros {
+    public static void main(String[] args) {
+        // print 
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+
+        // Ejemplo 
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
+
+        
+    }
+}
