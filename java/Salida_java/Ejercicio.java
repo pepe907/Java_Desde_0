73a5746f10eb9952da3ad3ab849696d012c9a8fa
Agustin Enrique Cabezas Yanguma <agustincabezas1454@gmail.com>
2026-03-24 23:06:00 -0500

    Ejercicios Print

---
+++ java/Salida_java/Ejercicio.java
@@ -0,0 +1,11 @@
+package java.Salida_java;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        /*Utilice print()o println()para imprimir su nombre
+        Utilice otro print()o println()para imprimir su edad */
+
+        System.out.println("Pepe ");
+        System.out.println(18);
+    }
+}
