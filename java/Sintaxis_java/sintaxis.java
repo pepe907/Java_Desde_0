155f132dd83f4ee8bfed793c1bf0b67b876c7d94
Agustin Enrique Cabezas Yanguma <agustincabezas1454@gmail.com>
2026-03-24 22:49:59 -0500

    SintaxisJava

---
+++ java/Sintaxis_java/sintaxis.java
@@ -0,0 +1,11 @@
+package java.Sintaxis_java;
+
+public class sintaxis {
+    public static void main(String[] args) {
+    System.out.println("Hello World"); // imprime hello world
+  }
+}
+
+ // public static void main(String[] args) Este main()método es necesario en todos los programas Java. Es donde comienza la ejecución del programa
+ 
+ 
