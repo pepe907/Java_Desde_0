69a6bd448be94bdc90fe0ef14849e976949b9194
Agustin Enrique Cabezas Yanguma <agustincabezas1454@gmail.com>
2026-03-24 22:29:13 -0500

    Inicio de java

---
+++ Introduccion_java/Inicio.java
@@ -0,0 +1,8 @@
+package Introduccion_java;
+
+public class Inicio {
+  public static void main(String[] args) {
+    String name = "John";
+    System.out.println("Hello " + name); // imprime john
+  }
+}
