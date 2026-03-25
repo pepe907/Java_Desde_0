03f09068f3563e2683bc4c5fa679f1403ce1c5af
Agustin Enrique Cabezas Yanguma <agustincabezas1454@gmail.com>
2026-03-24 22:33:06 -0500

    Primeros pasos

---
+++ Java_primeros_pasos/Primeros_pasos.java
@@ -0,0 +1,7 @@
+package Java_primeros_pasos;
+
+public class Primeros_pasos {
+    public static void main(String[] args) {
+         System.out.println("Hello World"); // imprime hello world
+    }
+}
