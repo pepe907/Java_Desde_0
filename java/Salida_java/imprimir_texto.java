43062ebacf365ee915c7f0f0cb434f7010f45942
Agustin Enrique Cabezas Yanguma <agustincabezas1454@gmail.com>
2026-03-24 23:02:11 -0500

    Imprimir texto

---
+++ java/Salida_java/imprimir_texto.java
@@ -0,0 +1,26 @@
+package java.Salida_java;
+
+public class imprimir_texto {
+    public static void main(String[] args) {
+        // Ejemplo
+        System.out.println("Hello World!");
+
+        // Ejemplo
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+
+        // Comillas dobles 
+        System.out.println("This sentence will work!");
+        // Sin comillas Dobles
+        System.out.println(This sentence will produce an error);
+
+        // El método Print()
+
+        // Ejemplo
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+
+        
+    }
+}
